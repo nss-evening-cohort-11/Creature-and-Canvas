9ca45346261ba8bc9501b0ae4c9d634e06cb9e5d
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-10-21 19:46:06 -0500

    added animal controller, animal repository, established SQL connection, and tested Get request

---
+++ Creature-and-Canvas/Controllers/AnimalsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Creature_and_Canvas.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Creature_and_Canvas.Controllers
+{
+    [Route("api/animals")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        AnimalRepository _repo;
+
+        public AnimalsController()
+        {
+            _repo = new AnimalRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllAnimals()
+        {
+            var allAnimals = _repo.GetAnimals();
+
+            return Ok(allAnimals);
+        }
+    }
+}
---
+++ Creature-and-Canvas/Models/Animal.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Creature_and_Canvas.Models
+{
+    public class Animal
+    {
+        public int AnimalID { get; set; }
+        public string AnimalName { get; set; }
+        public string Bio { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
