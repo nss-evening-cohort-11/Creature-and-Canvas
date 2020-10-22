9d8d5e2b32e004898d4821167acdecd86c196145
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-10-21 20:16:37 -0500

    added payment controller, model, and data, and tested GetById

---
+++ Creature-and-Canvas/Controllers/PaymentsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Creature_and_Canvas.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Creature_and_Canvas.Controllers
+{
+    [Route("api/payments")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        PaymentRepository _repo;
+
+        public PaymentsController()
+        {
+            _repo = new PaymentRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllPayments()
+        {
+            var allPayments = _repo.GetPayments();
+
+            return Ok(allPayments);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetPaymentById(int id)
+        {
+            var payment = _repo.GetPaymentById(id);
+
+            if (payment == null) return NotFound("No payment with that Id was found");
+
+            return Ok(payment);
+        }
+    }
+}
