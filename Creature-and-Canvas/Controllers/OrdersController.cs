900e65894ea7b790895f9f25474542b95874491b
Aaron Lain <aaron.j.lain@gmail.com>
2020-10-24 10:49:05 -0500

    order data complete

---
+++ Creature-and-Canvas/Controllers/OrdersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Creature_and_Canvas.Data;
+
+namespace Creature_and_Canvas.Controllers
+{
+    [Route("api/orders")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        OrderRepository _repo;
+
+        public OrdersController()
+        {
+            _repo = new OrderRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllOrders()
+        {
+            var allOrders = _repo.GetOrders();
+
+            return Ok(allOrders);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetOrderById(int id)
+        {
+            var order = _repo.GetOrderById(id);
+
+            if (order == null) return NotFound("No payment with that Id was found");
+
+            return Ok(order);
+        }
+    }
+}
---
+++ Creature-and-Canvas/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Creature_and_Canvas.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public int CustomerID { get; set; }
+        public int PaymentID { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
