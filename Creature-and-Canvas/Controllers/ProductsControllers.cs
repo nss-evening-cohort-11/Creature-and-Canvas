c6e3e2a198e68bcbeb9ca139a8f9d230b92639c8
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-10-24 10:42:43 -0500

    products controller, model, and repo. Tested Get request for All products and by ID.

---
+++ Creature-and-Canvas/Controllers/ProductsControllers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Creature_and_Canvas.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Creature_and_Canvas.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        ProductRepository _repo;
+
+        public ProductsController()
+        {
+            _repo = new ProductRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var allProducts = _repo.GetProducts();
+
+            return Ok(allProducts);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            var product = _repo.GetProductById(id);
+
+            if (product == null) return NotFound("No product with that Id was found");
+
+            return Ok(product);
+        }
+    }
+}
