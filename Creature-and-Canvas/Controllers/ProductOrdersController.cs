853eacac1e203a888c07abcbad67748f797b9f90
Aaron Lain <aaron.j.lain@gmail.com>
2020-10-24 11:16:37 -0500

    ProductOrders complete

---
+++ Creature-and-Canvas/Controllers/ProductOrdersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Creature_and_Canvas.Data;
+
+namespace Creature_and_Canvas.Controllers
+{
+    [Route("api/productOrders")]
+    [ApiController]
+    public class ProductOrdersController : ControllerBase
+    {
+        ProductOrderRepository _repo;
+
+        public ProductOrdersController()
+        {
+            _repo = new ProductOrderRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var allProductOrders = _repo.GetProductOrders();
+
+            return Ok(allProductOrders);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductOrderById(int id)
+        {
+            var productOrder = _repo.GetProductOrderById(id);
+
+            if (productOrder == null) return NotFound("No productOrder with that Id was found");
+
+            return Ok(productOrder);
+        }
+    }
+}
---
+++ Creature-and-Canvas/Data/ProductOrderRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using Creature_and_Canvas.Models;
+using System.Threading.Tasks;
+
+namespace Creature_and_Canvas.Data
+{
+    public class ProductOrderRepository
+    {
+
+        static List<ProductOrder> _productOrders = new List<ProductOrder>();
+
+        const string _connectionString = "Server=localhost;Database=Creature_and_Canvas;Trusted_Connection=True;";
+
+        public List<ProductOrder> GetProductOrders()
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            var productOrders = db.Query<ProductOrder>("select * from ProductOrders");
+
+            return productOrders.ToList();
+        }
+
+        public ProductOrder GetProductOrderById(int productOrderId)
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            var query = @"select *
+                          from ProductOrders
+                          where ProductOrderID = @poid";
+
+            var parameters = new { poid = productOrderId };
+
+            var productOrder = db.QueryFirstOrDefault<ProductOrder>(query, parameters);
+
+            return productOrder;
+        }
+    }
+}
