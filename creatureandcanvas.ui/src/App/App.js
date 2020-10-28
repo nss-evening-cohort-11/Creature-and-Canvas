ad406faa2bf601e08987c8ea6f8a39fa7f8b6549
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-10-28 17:57:09 -0500

    routing works

--- creatureandcanvas.ui/src/App/App.js
+++ creatureandcanvas.ui/src/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
-import './App/App.scss';
+import './App.scss';
 
 import Home from '../components/pages/Home/Home';
 import Animals from '../components/pages/Animals/Animals';
@@ -9,8 +9,18 @@ import SingleAnimal from '../components/shared/SingleAnimal/SingleAnimal';
 import Paintings from '../components/pages/Paintings/Paintings';
 import SinglePainting from '../components/shared/SinglePainting/SinglePainting';
 
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
 class App extends React.Component {
+  state = {authed: true,}
   render() {
+    const { authed } = this.state;
+
     return (
       <div className='App'>
         <BrowserRouter>
@@ -18,11 +28,11 @@ class App extends React.Component {
             <div className='container'>
               <div className='row'>
                 <Switch>
-                  <PublicRoute path='/home' exact component={Home} />
-                  <PublicRoute path='/animals' exact component={Animals} />
-                  <PublicRoute path='/animals/12345' exact component={SingleAnimal} />
-                  <PublicRoute path='/paintings' exact component={Paintings} />
-                  <PublicRoute path='/paintings/56789' exact component={SinglePainting} />
+                  <PublicRoute path='/home' exact component={Home} authed={authed}/>
+                  <PublicRoute path='/animals' exact component={Animals} authed={authed}/>
+                  <PublicRoute path='/animals/12345' exact component={SingleAnimal} authed={authed}/>
+                  <PublicRoute path='/paintings' exact component={Paintings} authed={authed}/>
+                  <PublicRoute path='/paintings/56789' exact component={SinglePainting} authed={authed}/>
                   <Redirect from='*' to='/home' />
                 </Switch>
               </div>
