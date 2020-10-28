8bf253ce5ce4618167348aa4b00a0aa9f4ee417b
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-10-28 16:27:53 -0500

    Added CORS to startup again

--- creatureandcanvas.ui/src/App/App.js
+++ creatureandcanvas.ui/src/App/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
-import './App/App.scss';
+import './App.scss';
 
 import Home from '../components/pages/Home/Home';
 import Animals from '../components/pages/Animals/Animals';
 import SingleAnimal from '../components/shared/SingleAnimal/SingleAnimal';
 import Paintings from '../components/pages/Paintings/Paintings';
 import SinglePainting from '../components/shared/SinglePainting/SinglePainting';
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
 
 class App extends React.Component {
   render() {
