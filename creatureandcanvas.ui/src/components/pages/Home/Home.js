4aa6ec0e0746c6bed5035ff2a95fbd3d082c5f8c
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-10-29 11:40:36 -0500

    filter buttons added to home page

--- creatureandcanvas.ui/src/components/pages/Home/Home.js
+++ creatureandcanvas.ui/src/components/pages/Home/Home.js
@@ -4,7 +4,7 @@ class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        <h1>Home</h1>
+        <h1 className="home mt-5">Home</h1>
         <button className="btn btn-outline-secondary mr-5">Filter By Animal</button>
         <button className="btn btn-outline-secondary">Filter By Size</button>
         <button className="btn btn-outline-secondary ml-5">Sort By Price</button>
