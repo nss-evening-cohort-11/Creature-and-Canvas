586c1aa02138b47f0a6d505f89edb5c2a7ff67b2
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-10-30 09:59:22 -0500

    added filter by animal button to Shop that links to Home component

--- creatureandcanvas.ui/src/components/pages/Shop/Shop.js
+++ creatureandcanvas.ui/src/components/pages/Shop/Shop.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Shop.scss';
+import { Link } from 'react-router-dom';
 
 class Shop extends React.Component {
   render() {
     return (
-      <div className="Shop">
-        <h1>Shop</h1>
+      <div className="Shop mt-3">
+        <h1 className="shop mb-5">Shop</h1>
+        <Link to='/home'><button className="btn btn-secondary">Filter By Animal</button></Link>
       </div>
     );
   }
