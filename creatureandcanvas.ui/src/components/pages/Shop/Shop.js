a48a398653eab429553685743d96d51ea697d6b9
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-10-30 09:44:35 -0500

    created Shop component, imported to App.js, created public route for shop, changed redirect to /shop and changed logo to link to /shop

---
+++ creatureandcanvas.ui/src/components/pages/Shop/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Shop.scss';
+
+class Shop extends React.Component {
+  render() {
+    return (
+      <div className="Shop">
+        <h1>Shop</h1>
+      </div>
+    );
+  }
+}
+
+export default Shop;
