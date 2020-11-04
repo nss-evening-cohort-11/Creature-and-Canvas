cfac14d7a28e4d21ef03902c782660ff2ce7280b
Aaron Lain <aaron.j.lain@gmail.com>
2020-11-03 18:22:06 -0600

    button & card sizing

--- creatureandcanvas.ui/src/components/pages/Paintings/Paintings.js
+++ creatureandcanvas.ui/src/components/pages/Paintings/Paintings.js
@@ -21,11 +21,12 @@ class Paintings extends React.Component {
     const { painting } = this.state;
     return (
       <div className="SinglePaintingView">
-      <div className="card mb-3">
+      <div className="card w-100">
         <div className="card-body">
-          <h5 className="card-title">{painting.Title}</h5>
+          <h5 className="card-title">{painting.title}</h5>
           <p className="card-text">{painting.paintingDescription}</p>
           <img src={painting.imageURL} alt="" className="card-img-bottom"/>
+          <button className="btn btn-secondary" onClick={() => console.log("Id: " + painting.itemId)}>Buy Immediately</button>
         </div>
       </div>
     </div>
