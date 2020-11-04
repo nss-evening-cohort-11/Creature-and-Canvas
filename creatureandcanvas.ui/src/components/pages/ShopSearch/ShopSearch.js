a6a562c967c491f08acc346ae4c4debdd408ced2
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-11-03 20:33:25 -0600

    fixed state issue, sql query not working with keyword

--- creatureandcanvas.ui/src/components/pages/ShopSearch/ShopSearch.js
+++ creatureandcanvas.ui/src/components/pages/ShopSearch/ShopSearch.js
@@ -6,22 +6,37 @@ import paintingsData from '../../../helpers/data/paintingsData';
 class ShopSearch extends React.Component {
 state = {
   searchValue: this.props.match.params,
-  newthing: {},  //working on setting state?
+  foundPainting: {},
+}
+
+getPaintingsByKeyword = () => {
+  const keyword = this.state.searchValue.keyword;
+  paintingsData.getPaintingsByKeyword(keyword)
+    .then((response) => this.setState({ foundPainting: response }))
+    //sets foundPainting as object = good
+    .catch((err) => console.error('could not get single memory:', err));
 }
 
 componentDidMount() {
-  const {keyword} = this.state.searchValue;
-  paintingsData.getPaintingsByKeyword(keyword)
-    .then((response) => this.setState({ newthing: response })) // not sure what to do with response?
-    .catch((err) => console.error('could not get single memory:', err));
-  console.log('search results:', this.state.newthing );
+  this.getPaintingsByKeyword();
+  console.log('componentMount:', this.state.foundPainting );
+  // this consoles a blank object, even though i can see the foundPainting is not blank in state
+  // is this something about the order/speed of retrieval?
+}
+
+getTitle = () => {
+  const title = this.state.foundPainting.title;
+  console.log('inside getTitle:', title);
+  return title;
 }
 
   render() {
+    const { painting } = this.state;
     return (
       <div className="ShopSearch mt-3">
         <h1 className="Search mb-5">Search Results</h1>
-        <h4>test search</h4>
+    <h4>h4 {this.getTitle()}</h4>
+    {/* <h3>h3 {painting.Title.foundPainting}</h3> */}
       </div>
     );
   }
