6625aee8431b4908cda0cb2e3264e0d94ceb44b9
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-11-21 09:21:10 -0600

    wip

--- creatureandcanvas.ui/src/components/shared/OrdersTable/OrdersTable.js
+++ creatureandcanvas.ui/src/components/shared/OrdersTable/OrdersTable.js
@@ -6,11 +6,13 @@ import {Link} from 'react-router-dom'
 class OrdersTable extends React.Component {
   render() {
     const { order } = this.props;
+    const dateProp = order.orderDate;
+    const shortDate = dateProp.substring(0, 10);
 
     return (
     <>
     <tr>
-      <td>{order.orderDate}</td>
+      <td>{shortDate}</td>
       <td>total</td>
       <Link to={`/orders/${order.orderID}`}>View Order</Link>
     </tr>
